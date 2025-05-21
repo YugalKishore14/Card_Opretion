674c2364a3886e0c6eebe70da3350c234eb9f74a
yugaldhiman <yugaldhiman14@gmail.com>
2025-05-21 16:46:13 +0530

    install react router dom and create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import ServicePage from './pages/ServicePage'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
     <>
-      <h1>Hello india</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/aboutpage' element={<AboutPage />} />
+          <Route path='/contactpage' element={< ContactPage />} />
+          <Route path='/servicepage' element={<ServicePage />} />
+          <Route path='/registerpage' element={<RegisterPage />} />
+          <Route path='/loginpage' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/ServicePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServicePage = () => {
+    return (
+        <div>
+            <h1>service page</h1>
+        </div>
+    )
+}
+
+export default ServicePage
