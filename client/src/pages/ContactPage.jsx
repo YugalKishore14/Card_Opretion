cd5c27b2e323e0213367ca42dc4ebbac1d39d11d
yugaldhiman <yugaldhiman14@gmail.com>
2025-05-30 13:12:34 +0530

    add google map in contact page

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './allCss/ContactPage.css'
+import { useState } from "react";
+import "./allCss/ContactPage.css";
 
 const ContactPage = () => {
     const [user, setUser] = useState({
-        email: '',
-        username: '',
-        message: ''
+        email: "",
+        username: "",
+        message: "",
     });
     const handleInput = (e) => {
         const name = e.target.name;
@@ -21,8 +21,7 @@ const ContactPage = () => {
         e.preventDefault();
         alert(user);
         console.log(user);
-
-    }
+    };
     return (
         <>
             <section>
@@ -30,58 +29,78 @@ const ContactPage = () => {
                     <div className="section-registration">
                         <div className="container grid grid-two-cols"></div>
                         <div className="registration-image">
-                            <img src="/images/support.png"
-                                alt="let's fill the login form" width={500} height={500} />
+                            <img
+                                src="/images/support.png"
+                                alt="let's fill the login form"
+                                width={500}
+                                height={500}
+                            />
                         </div>
                         {/* let tackel registration form */}
                         <div className="registration-form">
-                            <h1 className='main-heading mb-3 contact-h1'>Contact Us</h1>
+                            <h1 className="main-heading mb-3 contact-h1">Contact Us</h1>
                             <br />
                             <form onSubmit={handleSubmit}>
                                 <div>
                                     <label htmlFor="email">email</label>
-                                    <input type="email"
+                                    <input
+                                        type="email"
                                         name="email"
-                                        placeholder='enter your email'
-                                        id='email'
+                                        placeholder="enter your email"
+                                        id="email"
                                         required
-                                        autoComplete='off'
+                                        autoComplete="off"
                                         value={user.email}
                                         onChange={handleInput}
                                     />
                                 </div>
                                 <div>
-                                    <label htmlFor="username">Username</label>
-                                    <input type="text"
+                                    <label htmlFor="username">username</label>
+                                    <input
+                                        type="text"
                                         name="username"
-                                        placeholder='enter your username'
-                                        id='username'
+                                        placeholder="enter your username"
+                                        id="username"
                                         required
-                                        autoComplete='off'
+                                        autoComplete="off"
                                         value={user.username}
                                         onChange={handleInput}
                                     />
                                 </div>
                                 <div>
-                                    <label >Message</label>
-                                    <textarea type="text"
+                                    <label>message</label>
+                                    <textarea
+                                        type="text"
                                         required
-                                        name='message'
-                                        autoComplete='off'
+                                        name="message"
+                                        autoComplete="off"
                                         value={user.message}
                                         onChange={handleInput}
                                     />
                                 </div>
 
                                 <br />
-                                <button type='submit' className='btn btn-submit'>Submit</button>
+                                <button type="submit" className="btn btn-submit">
+                                    Submit
+                                </button>
                             </form>
                         </div>
+                    </div>
+                    <div>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111989.
+                            15941158684!2d77.3126275323735!3d28.699778769826178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+                            1!3m3!1m2!1s0x390cf1bb41c50fdf%3A0xe6f06fd26a7798ba!2sGhaziabad%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1748590143280!5m2!1sen!2sin"
+                            allowFullScreen
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="Contact Map"
+                        />
                     </div>
                 </main>
             </section>
         </>
-    )
-}
+    );
+};
 
-export default ContactPage
+export default ContactPage;
